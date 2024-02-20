6f1e32edfdbc8ffca47d7177f3e922409fcbf921
Joal Arcos <arcos@outlook.com>
2024-02-19 21:27:52 -0700

    Added Picker demo and gitignore

---
+++ PickerDemo/PickerDemo/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  PickerDemo
+//
+//  Created by jediOne on 2/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var colorType: ColorType = .Orange
+    
+    /// This works without setting tag.
+    var body: some View {
+        VStack {
+            Picker("Picker Title", selection: $colorType) {
+                ForEach(ColorType.allCases, id: \.self) {
+                    Text($0.rawValue).tag($0.rawValue)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+        }
+        .padding()
+    }
+}
+
+enum ColorType: String, CaseIterable {
+    case Orange
+    case Red
+    case Green
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PickerDemo/PickerDemo/PickerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerDemoApp.swift
+//  PickerDemo
+//
+//  Created by jediOne on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct PickerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
